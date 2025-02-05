f1f32939342659ec0272c74d212d00e8ee2b8b96
Lawrence Naumov <prostolawr@gmail.com>
2025-02-06 05:44:35 +0700

    Updtate to 0.2.8a0 with different databases

--- sqlalchemy_service/base_db/create.py
+++ sqlalchemy_service/base_db/create.py
@@ -1,75 +1,61 @@
 """Module with functions for prepare database for using"""
 
 import asyncio
-import os
-
-try:
-    import asyncpg as driver
-except ImportError:
-    driver = None
-
 from loguru import logger
-from pydantic_settings import BaseSettings
-from pydantic_settings import SettingsConfigDict
+
+from sqlalchemy_service.base_db.db_configure import DBConfigurator, DBConfigureInterface
+
+db_configurator = DBConfigurator()
 
 
-class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra="allow")
-
-    database_host: str = os.getenv('database_host')
-    database_db: str = os.getenv('database_db')
-    database_password: str = os.getenv('database_password')
-    database_user: str = os.getenv('database_user')
-    database_type: str = os.getenv('database_type', "postgres")
-    database_driver: str | None = os.getenv('database_driver', "asyncpg")
+class DriverNotFoundError(Exception):
+    def __init__(self):
+        super().__init__('Database driver was not found')
 
 
-settings = Settings()
+if db_configurator.configuration.get_db_type() == 'postgres':
+    import asyncpg as driver
+elif db_configurator.configuration.get_db_type() == 'mariadb':
+    import aiomysql as driver
+else:
+    raise DriverNotFoundError()
 
 
-async def connect_create_if_not_exists(user, database, password, host):
-    """Do a 20 attempts to connect and create database"""
+async def connect_create_if_not_exists(
+        db_configuration: DBConfigureInterface
+):
+    """Do 20 attempts to connect and create database"""
     for i in range(20):
         try:
-            conn = await driver.connect(
-                user=user, database=database,
-                password=password, host=host
-            )
+            conn = await driver.connect(db_configuration.get_url())
             await conn.close()
             break
         except driver.InvalidCatalogNameError:
             # Database does not exist, create it.
             sys_conn = await driver.connect(
-                database='template1',
-                user=user,
-                password=password,
-                host=host
+                db_configuration.get_url_with_default_db_name(),
             )
             await sys_conn.execute(
-                f'CREATE DATABASE "{database}" OWNER "{user}"'
+                f'CREATE DATABASE "{db_configuration.get_db_name()}" '
+                f'OWNER "{db_configuration.get_db_user()}"'
             )
             await sys_conn.close()
             break
         except Exception as e:
-            logger.exception(f"Error on database connection: {str(e)}\n[{i + 1}/20] Retry in 5 seconds...")
+            logger.exception(
+                f"Error on database connection: {str(e)}\n[{i + 1}/20] Retry in 5 seconds..."
+            )
             await asyncio.sleep(5)
 
 
 def run_init_db():
     """Run database creation in DMS. Use provided driver_name for driver import"""
-    global driver
-    if settings.database_driver:
-        driver = __import__(settings.database_driver)
-
     asyncio.run(
         connect_create_if_not_exists(
-            settings.database_user,
-            settings.database_db,
-            settings.database_password,
-            settings.database_host
+            db_configurator.configuration,
         )
     )
-    logger.info(f'DB initialization with name {settings.database_db} is done')
+    logger.info(f'DB initialization with name {db_configurator.configuration.get_db_name()} is done')
 
 
 if __name__ == '__main__':
--- sqlalchemy_service/base_db/db_configure.py
+++ sqlalchemy_service/base_db/db_configure.py
@@ -11,7 +11,19 @@ class DBConfigureInterface:
         ...
 
     @abstractmethod
-    def get_name(self) -> str:
+    def get_url_with_default_db_name(self) -> str:
+        ...
+
+    @abstractmethod
+    def get_db_type(self) -> str:
+        ...
+
+    @abstractmethod
+    def get_db_name(self) -> str:
+        ...
+
+    @abstractmethod
+    def get_db_user(self) -> str:
         ...
 
 
@@ -31,12 +43,13 @@ class DBConfigurationNotFoundError(Exception):
 
 
 class OldPostgresSQLDBConfiguration(DBConfigureInterface, BaseSettings):
+    postgres_env_warning: bool = True
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra="allow")
 
     pg_default_envs_link = "https://www.postgresql.org/docs/current/libpq-envars.html"
 
     postgres_host: str = os.getenv('POSTGRES_HOST', '')
-    postgres_db: str = os.getenv('POSTGRES_DB', '')
+    postgres_database: str = os.getenv('POSTGRES_DATABASE', '')
     postgres_port: str = os.getenv('POSTGRES_PORT', "5432")
     postgres_password: str = os.getenv('POSTGRES_PASSWORD', "postgres")
     postgres_user: str = os.getenv('POSTGRES_USER', "postgres")
@@ -53,13 +66,39 @@ class OldPostgresSQLDBConfiguration(DBConfigureInterface, BaseSettings):
             raise DBNameNotSetError()
         return f'postgres+asyncpg://' \
                f'{self.postgres_user}:{self.postgres_password}' \
-               f'@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}'
+               f'@{self.postgres_host}:{self.postgres_port}/{self.postgres_database}'
 
     def __str__(self):
         return self.get_url()
 
-    def get_name(self) -> str:
+    def get_db_type(self) -> str:
         return 'postgres'
+
+    def get_db_name(self) -> str:
+        return self.postgres_database
+
+    def get_db_user(self) -> str:
+        return self.postgres_user
+
+    def _validate(self):
+        if self.postgres_env_warning:
+            logger.warning(
+                "POSTGRES_<setting> is deprecated. "
+                "Please, use PG<setting>\n"
+                "See {pg_default_envs_link}\n"
+                "Set POSTGRES_ENV_WARNING=false to ignore it"
+            )
+            self.postgres_env_warning = False
+
+        if not self.postgres_host:
+            raise DBHostNotSetError()
+        if not self.postgres_database:
+            raise DBNameNotSetError()
+
+    def get_url_with_default_db_name(self) -> str:
+        return f'postgres+asyncpg://' \
+               f'{self.postgres_user}:{self.postgres_password}' \
+               f'@{self.postgres_host}:{self.postgres_port}/template1'
 
 
 class PostgresSQLDBConfiguration(DBConfigureInterface, BaseSettings):
@@ -85,9 +124,25 @@ class PostgresSQLDBConfiguration(DBConfigureInterface, BaseSettings):
     def __str__(self):
         return self.get_url()
 
-    def get_name(self) -> str:
+    def get_db_type(self) -> str:
         return 'postgres'
 
+    def get_db_name(self) -> str:
+        return self.pgdatabase
+
+    def get_db_user(self) -> str:
+        return self.pguser
+
+    def _validate(self):
+        if not self.pghost:
+            raise DBHostNotSetError()
+        if not self.pgdatabse:
+            raise DBNameNotSetError()
+
+    def get_url_with_default_db_name(self) -> str:
+        return f'postgres+asyncpg://' \
+               f'{self.pguser}:{self.pgpassword}' \
+               f'@{self.pghost}:{self.pgport}/template1'
 
 class MySQLDBConfiguration(DBConfigureInterface, BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8', extra="allow")
@@ -110,22 +165,26 @@ class MySQLDBConfiguration(DBConfigureInterface, BaseSettings):
     def __str__(self):
         return self.get_url()
 
-    def get_name(self) -> str:
+    def get_db_type(self) -> str:
         return 'mysql'
 
+    def get_db_name(self) -> str:
+        return self.mysql_db
+
+    def get_db_user(self) -> str:
+        return self.mysql_user
+
+    def get_url_with_default_db_name(self) -> str:
+        return f'mysql+aiomysql://' \
+               f'{self.mysql_user}:{self.mysql_password}' \
+               f'@{self.mysql_host}:{self.mysqlport}'
 
 class DBConfigurator:
     configuration_classes = [PostgresSQLDBConfiguration, MySQLDBConfiguration]
 
     def __init__(self):
-        self.configure_url = ''
+        self.configuration: DBConfigureInterface = DBConfigureInterface()
         self._try_configures()
-
-    def get_url(self) -> str:
-        return self.configure_url
-
-    def __str__(self):
-        return self.get_url()
 
     def _try_configures(self):
         for configuration_class in self.configuration_classes:
