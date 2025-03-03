01a1f0964b134b282d55cc9de34a91496f08dfe1
romakot321 <play62342@gmail.com>
2025-03-03 18:14:34 +0300

    .

--- sqlalchemy_service/base_db/db_configure.py
+++ sqlalchemy_service/base_db/db_configure.py
@@ -153,7 +153,7 @@ class MySQLDBConfiguration(DBConfigureInterface, BaseSettings):
             raise DBHostNotSetError('mysql')
         if not self.mysql_db:
             raise DBNameNotSetError('mysql')
-        return f'mysql+aiomysql://' \
+        return f'mysql+asyncmy://' \
                f'{self.mysql_user}:{self.mysql_password}' \
                f'@{self.mysql_host}:{self.mysql_port}/{self.mysql_db}'
 
@@ -170,7 +170,7 @@ class MySQLDBConfiguration(DBConfigureInterface, BaseSettings):
         return self.mysql_user
 
     def get_url_with_default_db_name(self) -> str:
-        return f'mysql+aiomysql://' \
+        return f'mysql+asyncmy://' \
                f'{self.mysql_user}:{self.mysql_password}' \
                f'@{self.mysql_host}:{self.mysqlport}'
 
